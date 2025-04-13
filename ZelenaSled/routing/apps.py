ee1c52325c5e1d086ba028d12711e61b9c860e55
Iva <iva_cernosa@hotmail.com>
2025-04-13 10:14:40 +0200

    add unzipig

--- ZelenaSled/routing/apps.py
+++ ZelenaSled/routing/apps.py
@@ -1,6 +1,24 @@
 from django.apps import AppConfig
-
+import zipfile
+import os
 
 class RoutingConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'routing'
+
+    def ready(self):
+        self.unzip_json_file()
+
+    def unzip_json_file(self):
+        # Define paths
+        zip_file_path = os.path.join(self.path, 'routing', 'data', 'avg_ndvi_h3_13.json.zip')
+        extracted_file_path = os.path.join(self.path, 'routing', 'data', 'avg_ndvi_h3_13.json')
+
+        # Only unzip if the JSON file doesn't already exist
+        if not os.path.exists(extracted_file_path):
+            with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
+                zip_ref.extract('avg_ndvi_h3_13.json', os.path.dirname(extracted_file_path))
+            print(f"Unzipped {zip_file_path} to {extracted_file_path}")
+        else:
+            print(f"JSON file already extracted: {extracted_file_path}")
+
